73fcb89cb4cf6b16445770bbc612b6364a5a3ea9
Timvanhal <s1115001@student.hsleiden.nl>
2019-11-01 14:46:46 +0100

    -Updated all methods in Journey, such that they work with authentication. -Changed config.yml -Changed pom.xml

--- src/main/java/com/ipsen2/api/IPSEN2_APIConfiguration.java
+++ src/main/java/com/ipsen2/api/IPSEN2_APIConfiguration.java
@@ -2,9 +2,32 @@ package com.ipsen2.api;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotEmpty;
 
 public class IPSEN2_APIConfiguration extends Configuration {
-    // TODO: implement service configuration
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
 }
